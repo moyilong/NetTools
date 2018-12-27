5265305f46683f6e31a8c344e7df03126f3e0758
moyilong <moyilong@foxmail.com>
2018-12-27 16:58:29 +0800

    update profile

--- 诊断工具/Loader.cs
+++ 诊断工具/Loader.cs
@@ -1,7 +1,8 @@
 ﻿using Phenom.Extension;
 using Phenom.ProgramMethod;
+using Phenom.UnionCore.API;
 using Phenom.WPF;
-
+using Phenom.UnionCore.APPAuth;
 using System;
 
 namespace 诊断工具
@@ -11,6 +12,9 @@ namespace 诊断工具
         [STAThread]
         private static void Main(string[] args)
         {
+            APIS.InitAPP("4b37a1736c5db70ebccd5add3f9dae27");
+            AuthProvider provider = new AuthProvider();
+            provider.Monited = true;
             PhenomWPF.QueryFormStart<App>(new QueryRunner.QueryRunnerItem[]
             {
             }, new Loading());
